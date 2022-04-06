c2a6fde6c21a284d91ce1e6e512b62823a045ff1
Unchaptered <workstation19961002@gmail.com>
2022-04-06 23:52:44 +0900

    REMOVE junk controller

--- src/auth/schema/user.schema.spec.ts
+++ src/auth/schema/user.schema.spec.ts
@@ -1,8 +1,34 @@
-import { User, UserDocument, UserSchema } from './user.schema';
+import { MongoClient, Db } from 'mongodb';
 
 describe('Mongoose Model Testing', () => {
 
-    test('hello', () => {
-        expect(1).toBe(1);
+    test('', ()=> {
+        expect(global.ATLAS_URL).toBe('h');
     })
-})
+    let mongoConnection;
+
+    beforeAll( async () => {
+        
+        mongoConnection = await MongoClient.connect(global.ATLAS_URL);
+
+    });
+
+    afterAll( async () => {
+        
+        await mongoConnection.close();
+
+    });
+
+    it('shoud insert a doc into collection', async () => {
+
+        const userDB = mongoConnection.collection('Users');
+
+        const mockingUserData = { email:'hello@email.com', password:'hi' };
+        await userDB.insertOne(mockingUserData);
+
+        const findUserData = await userDB.findOne({ email: 'hello@gmail.com' });
+        expect(findUserData).toEqual(mockingUserData);
+
+    });
+
+});
